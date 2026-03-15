33f19b267472a10ba85feb8efcf3d2370d2e2a28
yoljasro <saidaliyevjasur450@gmail.com>
2026-03-15 21:53:54 +0500

    dfd

--- routes/tarot.js
+++ routes/tarot.js
@@ -3,11 +3,11 @@ const {
     drawDailyCard,
     drawAgain
 } = require('../controllers/tarotController');
-const { protect } = require('../middleware/auth');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/daily', protect, drawDailyCard);
-router.post('/draw', protect, drawAgain);
+router.get('/daily', auth, drawDailyCard);
+router.post('/draw', auth, drawAgain);
 
 module.exports = router;
